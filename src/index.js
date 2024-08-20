a3053a05ad14970cdc112bf6dc80c7dedefdae9c
Bonnie444-888 <isabel.kedibone@gmail.com>
2024-08-20 16:31:03 +0200

    added new city in HTNL file and made JS code shorter

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 function ShowTimeZone(event) {
-  if (event.target.value === "paris") {
-    let parisTimeZone = moment()
-      .tz("Europe/Paris")
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
       .format("dddd, MMMM D, YYYY h:mm A");
-    alert(`It is ${parisTimeZone} in Europe/Paris `);
-  }
-  if (event.target.value === "tokyo") {
-    let tokyoTimeZone = moment()
-      .tz("Asia/Tokyo")
-      .format("dddd, MMMM D, YYYY h:mm A");
-    alert(`It is ${tokyoTimeZone} in Asia/Tokyo `);
-  }
-  if (event.target.value === "sydney") {
-    let sydneyTimeZone = moment()
-      .tz("Australia/Sydney")
-      .format("dddd, MMMM D, YYYY h:mm A");
-    alert(`It is ${sydneyTimeZone} in Australia/Sydney `);
+    alert(`It is ${currentTime} in ${event.target.value} `);
   }
 }
 
